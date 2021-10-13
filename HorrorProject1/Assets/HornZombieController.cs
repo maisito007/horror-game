25afd05f2389e588ff5e9a2ed6c22fa86e3a29e1
maisito007 <media.mike.diaz@gmail.com>
2021-10-13 18:12:57 -0400

    initial commit
    current state of project

---
+++ HorrorProject1/Assets/HornZombieController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HornZombieController : MonoBehaviour
+{
+    public Transform moveTo;
+    public Animator thisAnim;
+    public float speed, speedLookAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAnim.SetBool("Idle", true);
+        thisAnim.SetBool("Run", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(MoveZombieToPos(moveTo.position));
+           
+        }
+    }
+    public void MoveZombie()
+    {
+        StartCoroutine(MoveZombieToPos(moveTo.position));
+    }
+    IEnumerator MoveZombieToPos(Vector3 moveToPos)
+    {
+         thisAnim.SetBool("Idle", false);
+        thisAnim.SetBool("Run", true);
+        while(this.transform.position != moveToPos)
+        //this.transform.position.x < baitPos.x -1f
+        {
+           
+            Debug.Log("Moving");
+            this.transform.position = Vector3.MoveTowards(this.transform.position, moveToPos, Time.deltaTime * speed);
+
+            // Instant LookAt()
+            // Vector3 baitLookAtPos = new Vector3(baitPos.x,this.transform.position.y, baitPos.z);
+            // this.transform.LookAt(baitLookAtPos);
+            var rotation = Quaternion.LookRotation(moveToPos - this.transform.position);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.deltaTime * speedLookAt);
+
+            // Vector3 eulerRotation = new Vector3(Camera.main.transform.eulerAngles.x, this.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z);
+            // Camera.main.transform.rotation = Quaternion.Euler(eulerRotation);
+
+
+            yield return null;
+        }
+    }
+}
---
+++ HorrorProject1/Assets/LightTriggerObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTriggerObject : MonoBehaviour
+{
+    public Light redRoomLight1, whiteHallWayLight1;
+    public HornZombieController zomControl1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LightsOn()
+    {
+        redRoomLight1.enabled = true;
+        whiteHallWayLight1.enabled = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ZombieEvent1")
+        {
+            LightsOn();
+            zomControl1.MoveZombie();
+
+        }
+    }
+}
